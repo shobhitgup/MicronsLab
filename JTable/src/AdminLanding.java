338441121a0316019f5f609bb9a9b48b159e3253
shobhitgup <shobhitgup@gmail.com>
2014-01-20 23:47:23 +0100

    Code Update

--- JTable/src/AdminLanding.java
+++ JTable/src/AdminLanding.java
@@ -1,71 +1,273 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 import net.miginfocom.swing.MigLayout;
+ 
+@SuppressWarnings("serial")
+public class AdminLanding extends JPanel 
+                               implements ListSelectionListener {
+    private JList listUser;
+    private JList listProject;
+    private DefaultListModel listModelUser;
+    private DefaultListModel listModelProject;
+    private static final String addString = "Add";
+    private static final String deleteString = "Delete";
+    private JButton addButton;
+    private JButton addButton1;
+    private JButton deleteButton;
+    private JButton deleteButton1;
+    private JTextField nameFieldUser;
+    private JTextField nameFieldProject;
+ 
+    public AdminLanding() {
+        super(new  MigLayout());
+ 
+        //Create and populate the user list model.
+        listModelUser = new DefaultListModel();
+        listModelUser.addElement("Dummy User");
+ 
+        //Create the user list and put it in a scroll pane.
+        listUser = new JList(listModelUser);
+        listUser.setSelectionMode(
+            ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        listUser.setSelectedIndex(0);
+        listUser.setVisibleRowCount(5);
+        listUser.addListSelectionListener(this);
+        listUser.setLayoutOrientation(JList.VERTICAL);
+        listUser.setBorder(BorderFactory.createLoweredBevelBorder());
+        JScrollPane listScrollPaneUser = new JScrollPane(listUser);
+        listScrollPaneUser.setMinimumSize(new Dimension(100, 50));
+        
+        //Create and populate the project list model.
+        listModelProject = new DefaultListModel();
+        listModelProject.addElement("Dummy Project");
+ 
+        //Create the project list and put it in a scroll pane.
+        listProject = new JList(listModelProject);
+        listProject.setSelectionMode(
+            ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        listProject.setSelectedIndex(0);
+        listProject.setVisibleRowCount(5);
+        listProject.addListSelectionListener(this);
+        listProject.setLayoutOrientation(JList.VERTICAL);
+        listProject.setBorder(BorderFactory.createLoweredBevelBorder());
+        JScrollPane listScrollPanelProject = new JScrollPane(listProject);
+        listScrollPanelProject.setMinimumSize(new Dimension(100, 50));
 
+ 
+        //Create the list-modifying buttons.
+        addButton = new JButton(addString);
+        addButton.setActionCommand(addString);
+        addButton.addActionListener(new AddButtonListener());
+        
+        addButton1 = new JButton(addString);
+        addButton1.setActionCommand(addString);
+        addButton1.addActionListener(new AddButtonListener1());
+ 
+        deleteButton = new JButton(deleteString);
+        deleteButton.setActionCommand(deleteString);
+        deleteButton.addActionListener(new DeleteButtonListener());
 
-public class AdminLanding {
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-            	createAndShowGUIAdminLogin();
+        deleteButton1 = new JButton(deleteString);
+        deleteButton1.setActionCommand(deleteString);
+        deleteButton1.addActionListener(new DeleteButtonListener1());
+
+        
+        //Create the text field for entering new user names.
+        nameFieldUser = new JTextField(15);
+        nameFieldUser.addActionListener(new AddButtonListener());
+        String nameUser = listModelUser.getElementAt(listUser.getSelectedIndex())
+                               .toString();
+        nameFieldUser.setText(nameUser);
+        
+        //Create the text field for entering new user project.
+        nameFieldProject = new JTextField(15);
+        nameFieldProject.addActionListener(new AddButtonListener1());
+        String nameProject = listModelProject.getElementAt(listProject.getSelectedIndex())
+                               .toString();
+        nameFieldProject.setText(nameProject);
+ 
+        //Create a control panel, using the default FlowLayout.
+        
+        MigLayout layout = new MigLayout();
+        JPanel buttonPane = new JPanel(layout);
+        JLabel User;
+        JLabel Project;
+		buttonPane.add(User = new JLabel("User"),"Pos 100 20 0 0");
+		buttonPane.add(Project = new JLabel("Project"),"Pos 300 20 0 0");
+        buttonPane.add(listScrollPaneUser,"Pos 100 40 0 0, width 150!, height 90!");
+        buttonPane.add(listScrollPanelProject,"Pos 300 40 0 0, width 150!, height 90!");
+        buttonPane.add(addButton,"Pos 100 160 0 0, width 70!, height 20!");
+        buttonPane.add(addButton1,"Pos 300 160 0 0, width 70!, height 20!");
+        buttonPane.add(nameFieldUser,"Pos 100 140 0 0, width 150!");
+        buttonPane.add(nameFieldProject,"Pos 300 140 0 0, width 150!");
+        buttonPane.add(deleteButton,"Pos 170 160 0 0, width 70!, height 20!");
+        buttonPane.add(deleteButton1,"Pos 370 160 0 0, width 70!, height 20!");
+ 
+        add(buttonPane);
+    }
+ 
+    class DeleteButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            ListSelectionModel lsm = listUser.getSelectionModel();
+            int firstSelected = lsm.getMinSelectionIndex();
+            int lastSelected = lsm.getMaxSelectionIndex();
+            listModelUser.removeRange(firstSelected, lastSelected);
+ 
+            int size = listModelUser.size();
+ 
+            if (size == 0) {
+                deleteButton.setEnabled(false);
+            } else {
+                if (firstSelected == listModelUser.getSize()) {
+                    firstSelected--;
+                }
+                listUser.setSelectedIndex(firstSelected);
             }
-        });    
+        }
     }
 
-    static void createAndShowGUIAdminLogin() {
-        MigLayout layout = new MigLayout();
-        JPanel panel = new JPanel(layout);
-        
-        JLabel UserList = new JLabel("Existing Users");
-        JLabel ProjectList = new JLabel("Existing Projects");
-        panel.add(UserList,"pos 150 50 0 0");
-        panel.add(ProjectList,"pos 250 50 0 0");
-        
-        DefaultListModel listModel = new DefaultListModel();
-        listModel.addElement("Jane Doe");
-        listModel.addElement("John Smith");
-        listModel.addElement("Kathy Green");
-        listModel.addElement("Shobhit");
-        JList list = new JList(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.setSelectedIndex(0);
-        list.setVisibleRowCount(5);
-        panel.add(list,"pos 150 80 0 0");
+    class DeleteButtonListener1 implements ActionListener {
+        public void actionPerformed(ActionEvent b) {
+            ListSelectionModel lsm1 = listProject.getSelectionModel();
+            int firstSelected1 = lsm1.getMinSelectionIndex();
+            int lastSelected1 = lsm1.getMaxSelectionIndex();
+            listModelProject.removeRange(firstSelected1, lastSelected1);
+            int size1 = listModelProject.size();
+            if (size1 == 0) {
+                deleteButton1.setEnabled(false);
+            } else {
+                if (firstSelected1 == listModelProject.getSize()) {
+                    firstSelected1--;
+                }
+                listProject.setSelectedIndex(firstSelected1);
+            }
+        }
+    }
 
-        DefaultListModel listModel1 = new DefaultListModel();
-        listModel1.addElement("Jane Doe");
-        listModel1.addElement("John Smith");
-        listModel1.addElement("Kathy Green");
-        JList list1 = new JList(listModel1);
-        list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list1.setSelectedIndex(0);
-        list1.setVisibleRowCount(5);
-        panel.add(list1,"pos 250 80 0 0");
+    
+    /** A listener shared by the text field and add button. */
+    class AddButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent c) {
+            if (nameFieldUser.getText().equals("")) {
+            //User didn't type in a name...
+                Toolkit.getDefaultToolkit().beep();
+                return;
+            }
+            
+            int index2 = listUser.getSelectedIndex();
+            int size2 = listModelUser.getSize();
+
+            //If no selection or if item in last position is selected,
+            //add the new one to end of list, and select new one.
+            if (index2 == -1 || (index2+1 == size2)) {
+            	listModelUser.addElement(nameFieldUser.getText());
+                listUser.setSelectedIndex(size2);} else {
+            	listModelUser.insertElementAt(nameFieldUser.getText(), index2+1);
+                listUser.setSelectedIndex(index2+1);
+            }
+        }
+    }
+ 
+    class AddButtonListener1 implements ActionListener {
+        public void actionPerformed(ActionEvent d) {
+            
+            if (nameFieldProject.getText().equals("")) {
+            //User didn't type in a name...
+                Toolkit.getDefaultToolkit().beep();
+                return;
+            }
+ 
+            int index3 = listProject.getSelectedIndex();
+            int size3 = listModelProject.getSize();
+ 
+            //If no selection or if item in last position is selected,
+            //add the new one to end of list, and select new one.
+            if (index3 == -1 || (index3+1 == size3)) {
+            	listModelProject.addElement(nameFieldProject.getText());
+                listProject.setSelectedIndex(size3);} else {
+                listModelProject.insertElementAt(nameFieldProject.getText(), index3+1);
+                listProject.setSelectedIndex(index3+1);
+            }
+        }
+    }
+ 
+    //Listener method for list selection changes.
+    public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting() == false) {
+ 
+            if (listUser.getSelectedIndex() == -1 ) {
+            //No selection: disable delete, up, and down buttons.
+                deleteButton.setEnabled(false);
+                nameFieldUser.setText("");
+ 
+            } else if (listUser.getSelectedIndices().length > 1) {
+            //Multiple selection: disable up and down buttons.
+                deleteButton.setEnabled(true);
+ 
+            } else {
+            //Single selection: permit all operations.
+                deleteButton.setEnabled(true);
+                nameFieldUser.setText(listUser.getSelectedValue().toString());
+            }
+        }
         
-        JFrame frame = new JFrame("Admin Login");
-        frame.setLocationRelativeTo(null);
+
+        
+        if (listProject.getSelectedIndex() == -1) {
+            //No selection: disable delete, up, and down buttons.
+                deleteButton1.setEnabled(false);
+                nameFieldProject.setText("");
+ 
+            } else if (listProject.getSelectedIndices().length > 1) {
+            //Multiple selection: disable up and down buttons.
+                deleteButton1.setEnabled(true);
+ 
+            } else {
+            //Single selection: permit all operations.
+                deleteButton1.setEnabled(true);
+                nameFieldProject.setText(listProject.getSelectedValue().toString());
+            }
+    
+    }
+ 
+    /** 
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    static void createAndShowGUIAdminLogin() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("ListDataEventDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(500,500));
-        frame.setLayout(new BorderLayout());
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setResizable(false);
-
+        //Create and set up the content pane.
+        JComponent newContentPane = new AdminLanding();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+         
+        //Don't let the content pane get too small.
+        //(Works if the Java look and feel provides
+        //the window decorations.)
+        newContentPane.setMinimumSize(
+                new Dimension(
+                        newContentPane.getPreferredSize().width,
+                        100));
+ 
+        //Display the window.
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	createAndShowGUIAdminLogin();
+            }
+        });
     }
 }

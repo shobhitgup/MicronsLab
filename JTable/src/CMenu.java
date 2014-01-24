e251cf147d48f40fd0b3a1093eb9ad508ca31afc
shobhitgup <shobhitgup@gmail.com>
2014-01-24 15:16:53 +0100

    Menu

--- JTable/src/CMenu.java
+++ JTable/src/CMenu.java
@@ -1,323 +1,216 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-// Import the Java classes
-import java.util.*;
-
-/**
- * A JOutlookBar provides a component that is similar to a JTabbedPane, but instead of maintaining
- * tabs, it uses Outlook-style bars to control the visible component
- */
-public class CMenu extends JPanel implements ActionListener
-{
-  /**
-   * The top panel: contains the buttons displayed on the top of the JOutlookBar
-   */
-  private JPanel topPanel = new JPanel( new GridLayout( 1, 1 ) );
-
-  /**
-   * The bottom panel: contains the buttons displayed on the bottom of the JOutlookBar
-   */
-  private JPanel bottomPanel = new JPanel( new GridLayout( 1, 1 ) );
-
-  /**
-   * A LinkedHashMap of bars: we use a linked hash map to preserve the order of the bars
-   */
-  private Map bars = new LinkedHashMap();
-
-  /**
-   * The currently visible bar (zero-based index)
-   */
-  private int visibleBar = 0;
-
-  /**
-   * A place-holder for the currently visible component
-   */
-  private JComponent visibleComponent = null;
-
-  /**
-   * Creates a new JOutlookBar; after which you should make repeated calls to
-   * addBar() for each bar
-   */
-  public CMenu()
-  {
-    this.setLayout( new BorderLayout() );
-    this.add( topPanel, BorderLayout.NORTH );
-    this.add( bottomPanel, BorderLayout.SOUTH );
-  }
-
-  /**
-   * Adds the specified component to the JOutlookBar and sets the bar's name
-   * 
-   * @param  name      The name of the outlook bar
-   * @param  componenet   The component to add to the bar
-   */
-  public void addBar( String name, JComponent component )
-  {
-    BarInfo barInfo = new BarInfo( name, component );
-    barInfo.getButton().addActionListener( this );
-    this.bars.put( name, barInfo );
-    render();
-  }
-
-  /**
-   * Adds the specified component to the JOutlookBar and sets the bar's name
-   * 
-   * @param  name      The name of the outlook bar
-   * @param  icon      An icon to display in the outlook bar
-   * @param  componenet   The component to add to the bar
-   */
-  public void addBar( String name, Icon icon, JComponent component )
-  {
-    BarInfo barInfo = new BarInfo( name, icon, component );
-    barInfo.getButton().addActionListener( this );
-    this.bars.put( name, barInfo );
-    render();
-  }
-
-  /**
-   * Removes the specified bar from the JOutlookBar
-   * 
-   * @param  name  The name of the bar to remove
-   */
-  public void removeBar( String name )
-  {
-    this.bars.remove( name );
-    render();
-  }
-
-  /**
-   * Returns the index of the currently visible bar (zero-based)
-   * 
-   * @return The index of the currently visible bar
-   */
-  public int getVisibleBar()
-  {
-    return this.visibleBar;
-  }
-
-  /**
-   * Programmatically sets the currently visible bar; the visible bar
-   * index must be in the range of 0 to size() - 1
-   * 
-   * @param  visibleBar   The zero-based index of the component to make visible
-   */
-  public void setVisibleBar( int visibleBar )
-  {
-    if( visibleBar > 0 &&
-      visibleBar < this.bars.size() - 1 )
-    {
-      this.visibleBar = visibleBar;
-      render();
-    }
-  }
-
-  /**
-   * Causes the outlook bar component to rebuild itself; this means that
-   * it rebuilds the top and bottom panels of bars as well as making the
-   * currently selected bar's panel visible
-   */
-  public void render()
-  {
-    // Compute how many bars we are going to have where
-    int totalBars = this.bars.size();
-    int topBars = this.visibleBar + 1;
-    int bottomBars = totalBars - topBars;
-
-
-    // Get an iterator to walk through out bars with
-    Iterator itr = this.bars.keySet().iterator();
-
-
-    // Render the top bars: remove all components, reset the GridLayout to
-    // hold to correct number of bars, add the bars, and "validate" it to
-    // cause it to re-layout its components
-    this.topPanel.removeAll();
-    GridLayout topLayout = ( GridLayout )this.topPanel.getLayout();
-    topLayout.setRows( topBars );
-    BarInfo barInfo = null;
-    for( int i=0; i<topBars; i++ )
-    {
-      String barName = ( String )itr.next();
-      barInfo = ( BarInfo )this.bars.get( barName );
-      this.topPanel.add( barInfo.getButton() );
-    }
-    this.topPanel.validate();
-
-
-    // Render the center component: remove the current component (if there
-    // is one) and then put the visible component in the center of this panel
-    if( this.visibleComponent != null )
-    {
-      this.remove( this.visibleComponent );
-    }
-    this.visibleComponent = barInfo.getComponent();
-    this.add( visibleComponent, BorderLayout.CENTER );
-
-
-    // Render the bottom bars: remove all components, reset the GridLayout to
-    // hold to correct number of bars, add the bars, and "validate" it to
-    // cause it to re-layout its components
-    this.bottomPanel.removeAll();
-    GridLayout bottomLayout = ( GridLayout )this.bottomPanel.getLayout();
-    bottomLayout.setRows( bottomBars );
-    for( int i=0; i<bottomBars; i++ )
-    {
-      String barName = ( String )itr.next();
-      barInfo = ( BarInfo )this.bars.get( barName );
-      this.bottomPanel.add( barInfo.getButton() );
-    }
-    this.bottomPanel.validate();
-
-
-    // Validate all of our components: cause this container to re-layout its subcomponents
-    this.validate();
-  }
-
-  /**
-   * Invoked when one of our bars is selected
-   */
-  public void actionPerformed( ActionEvent e )
-  {
-    int currentBar = 0;
-    for( Iterator i=this.bars.keySet().iterator(); i.hasNext(); )
-    {
-      String barName = ( String )i.next();
-      BarInfo barInfo = ( BarInfo )this.bars.get( barName );
-      if( barInfo.getButton() == e.getSource() )
-      {
-        // Found the selected button
-        this.visibleBar = currentBar;
-        render();
-        return;
-      }
-      currentBar++;
-    }
-  }
-
-  /**
-   * Debug, dummy method
-   */
-  public static JPanel getDummyPanel( String name )
-  {
-    JPanel panel = new JPanel( new BorderLayout() );
-    panel.add( new JLabel( name, JLabel.CENTER ) );
-    return panel;
-  }
-
-  /**
-   * Debug test...
-   */
-  public static void main( String[] args )
-  {
-    JFrame frame = new JFrame( "JOutlookBar Test" );
-    CMenu outlookBar = new CMenu();
-    outlookBar.addBar( "One", getDummyPanel( "One" ) );
-    outlookBar.addBar( "Two", getDummyPanel( "Two" ) );
-    outlookBar.addBar( "Three", getDummyPanel( "Three" ) );
-    outlookBar.addBar( "Four", getDummyPanel( "Four" ) );
-    outlookBar.addBar( "Five", getDummyPanel( "Five" ) );
-    outlookBar.setVisibleBar( 2 );
-    frame.getContentPane().add( outlookBar );
-
-    frame.setSize( 800, 600 );
-    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-    frame.setLocation( d.width / 2 - 400, d.height / 2 - 300 );
-    frame.setVisible( true );
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-  }
-
-  /**
-   * Internal class that maintains information about individual Outlook bars;
-   * specifically it maintains the following information:
-   * 
-   * name      The name of the bar
-   * button     The associated JButton for the bar
-   * component    The component maintained in the Outlook bar
-   */
-  class BarInfo
-  {
-    /**
-     * The name of this bar
-     */
-    private String name;
-
-    /**
-     * The JButton that implements the Outlook bar itself
-     */
-    private JButton button;
-
-    /**
-     * The component that is the body of the Outlook bar
-     */
-    private JComponent component;
-
-    /**
-     * Creates a new BarInfo
-     * 
-     * @param  name    The name of the bar
-     * @param  component  The component that is the body of the Outlook Bar
-     */
-    public BarInfo( String name, JComponent component )
-    {
-      this.name = name;
-      this.component = component;
-      this.button = new JButton( name );
-    }
-
-    /**
-     * Creates a new BarInfo
-     * 
-     * @param  name    The name of the bar
-     * @param  icon    JButton icon
-     * @param  component  The component that is the body of the Outlook Bar
-     */
-    public BarInfo( String name, Icon icon, JComponent component )
-    {
-      this.name = name;
-      this.component = component;
-      this.button = new JButton( name, icon );
-    }
-
-    /**
-     * Returns the name of the bar
-     * 
-     * @return The name of the bar
-     */
-    public String getName()
-    {
-      return this.name;
-    }
-
-    /**
-     * Sets the name of the bar
-     * 
-     * @param  The name of the bar
-     */
-    public void setName( String name )
-    {
-      this.name = name;
-    }
-
-    /**
-     * Returns the outlook bar JButton implementation
-     * 
-     * @return   The Outlook Bar JButton implementation
-     */
-    public JButton getButton()
-    {
-      return this.button;
-    }
-
-    /**
-     * Returns the component that implements the body of this Outlook Bar
-     * 
-     * @return The component that implements the body of this Outlook Bar
-     */
-    public JComponent getComponent()
-    {
-      return this.component;
-    }
-  }
-}
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;  
+       
+    public class CMenu {  
+        private JPanel getContent() {  
+            JPanel panel = new JPanel(new GridBagLayout());  
+            GridBagConstraints gbc = new GridBagConstraints();  
+            panel.add(new AccordianPanel().getPanel(), gbc);  
+            return panel;  
+        }  
+       
+        public static void main(String[] args) {  
+            JFrame f = new JFrame();  
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+            f.getContentPane().add(new CMenu().getContent());  
+            f.setSize(400,400);  
+            f.setLocation(200,200);  
+            f.setVisible(true);  
+        }  
+    }  
+       
+    class AccordianPanel extends JPanel {  
+        boolean movingComponents = false;  
+        int visibleIndex = 3;  
+       
+        public AccordianPanel() {  
+            setLayout(null);  
+            // Add children and compute prefSize.  
+            int childCount = 4;  
+            Dimension d = new Dimension();  
+            int h = 0;  
+            for(int j = 0; j < childCount; j++) {  
+                ChildPanel child = new ChildPanel(j+1, ml);  
+                add(child);  
+                d = child.getPreferredSize();  
+                child.setBounds(0, h, d.width, d.height);  
+                if(j < childCount-1)  
+                    h += ControlPanel.HEIGHT;  
+            }  
+            h += d.height;  
+            setPreferredSize(new Dimension(d.width, h));  
+            // Set z-order for children.  
+            setZOrder();  
+        }  
+       
+        private void setZOrder() {  
+            Component[] c = getComponents();  
+            for(int j = 0; j < c.length-1; j++) {  
+                setComponentZOrder(c[j], c.length-1 - j);  
+            }  
+        }  
+       
+        private void setChildVisible(int indexToOpen) {  
+            // If visibleIndex < indexToOpen, components at  
+            // [visibleIndex+1 down to indexToOpen] move up.  
+            // If visibleIndex > indexToOpen, components at  
+            // [indexToOpen+1 up to visibleIndex] move down.  
+            // Collect indices of components that will move  
+            // and determine the distance/direction to move.  
+            int[] indices = new int[0];  
+            int travelLimit = 0;  
+            if(visibleIndex < indexToOpen) {  
+                travelLimit = ControlPanel.HEIGHT -  
+                                  getComponent(visibleIndex).getHeight();  
+                int n = indexToOpen - visibleIndex;  
+                indices = new int[n];  
+                for(int j = visibleIndex, k = 0; j < indexToOpen; j++, k++)  
+                    indices[k] = j + 1;  
+            } else if(visibleIndex > indexToOpen) {  
+                travelLimit = getComponent(visibleIndex).getHeight() -  
+                                  ControlPanel.HEIGHT;  
+                int n = visibleIndex - indexToOpen;  
+                indices = new int[n];  
+                for(int j = indexToOpen, k = 0; j < visibleIndex; j++, k++)  
+                    indices[k] = j + 1;  
+            }  
+            movePanels(indices, travelLimit);  
+            visibleIndex = indexToOpen;  
+        }  
+       
+        private void movePanels(final int[] indices, final int travel) {  
+            movingComponents = true;  
+            Thread thread = new Thread(new Runnable() {  
+                public void run() {  
+                    Component[] c = getComponents();  
+                    int limit = travel > 0 ? travel : 0;  
+                    int count = travel > 0 ? 0 : travel;  
+                    int dy    = travel > 0 ? 8 : -8;  
+                    System.out.println("-----travel="+travel);
+                    System.out.println("--count---="+count);
+                    System.out.println("-limit-"+limit);
+                    
+                    while(count < limit) {  
+                        try {  
+                            Thread.sleep(25);  
+                        } catch(InterruptedException e) {  
+                            System.out.println("interrupted");  
+                            break;  
+                        }  
+                        for(int j = 0; j < indices.length; j++) {  
+                            
+                            // The z-order reversed the order returned  
+                            // by getComponents. Adjust the indices to  
+                            // get the correct components to relocate.  
+                            int index = c.length-1 - indices[j];  
+                            Point p = c[index].getLocation();  
+                            p.y += dy;  
+                            c[index].setLocation(p.x, p.y);  
+                            System.out.println("x="+p.x+"y="+p.y);
+                        }  
+                        repaint();  
+                        count=count+8;  
+                    }  
+                    movingComponents = false;  
+                }  
+            });  
+            thread.setPriority(Thread.NORM_PRIORITY);  
+            thread.start();  
+        }  
+       
+        private MouseListener ml = new MouseAdapter() {  
+            public void mousePressed(MouseEvent e) {  
+                int index = ((ControlPanel)e.getSource()).id-1;  
+                if(!movingComponents)  
+                    setChildVisible(index);  
+            }  
+        };  
+       
+        public JPanel getPanel() {  
+            JPanel panel = new JPanel(new GridBagLayout());  
+            GridBagConstraints gbc = new GridBagConstraints();  
+            panel.setBorder(BorderFactory.createLineBorder(Color.black, 1));  
+            panel.add(this, gbc);  
+            return panel;  
+        }  
+    }  
+       
+    class ChildPanel extends JPanel {  
+        public ChildPanel(int id, MouseListener ml) {  
+            setLayout(new BorderLayout());  
+            add(new ControlPanel(id, ml), "First");  
+            add(getContent(id));  
+        }  
+       
+        private JPanel getContent(int id) {  
+            JPanel panel = new JPanel(new GridBagLayout());
+            System.out.println("ID :" + id);
+            GridBagConstraints gbc = new GridBagConstraints();  
+            gbc.insets = new Insets(2,2,2,2);  
+            gbc.weightx = 1.0;  
+            gbc.weighty = 1.0;  
+            gbc.anchor = gbc.NORTHWEST;  
+            
+            if (id == 1) panel.add(new JLabel("Suite A"), gbc);  
+            if (id == 2) panel.add(new JLabel("Confitugation"), gbc);
+            if (id == 3) panel.add(new JLabel("Req 1"), gbc);
+            if (id == 4) panel.add(new JLabel("Report 1"), gbc);
+            
+            //panel.add(new JLabel("Panel " + id + " Content"), gbc);  
+            return panel;  
+        }  
+       
+        public Dimension getPreferredSize() {  
+            return new Dimension(300,150);  
+        }  
+    }  
+       
+    class ControlPanel extends JPanel {  
+        int id;  
+        JLabel titleLabel;  
+        Color c1 = new Color(200,180,180);  
+        Color c2 = new Color(200,220,220);  
+        Color fontFg = Color.blue;  
+        Color rolloverFg = Color.red;  
+        public final static int HEIGHT = 45;  
+       
+        public ControlPanel(int id, MouseListener ml) {  
+            this.id = id;  
+            //System.out.println("ID :" + id);
+            setLayout(new BorderLayout());  
+            
+            //add(titleLabel = new JLabel("PanelSho " + id, JLabel.CENTER));  
+            
+            if (id == 1) add(titleLabel = new JLabel("Test Management", JLabel.CENTER));
+            if (id == 2) add(titleLabel = new JLabel("Execute", JLabel.CENTER)); 
+            if (id == 3) add(titleLabel = new JLabel("Requirements", JLabel.CENTER));
+            if (id == 4) add(titleLabel = new JLabel("Reports", JLabel.CENTER)); 
+            	
+            titleLabel.setForeground(fontFg);  
+            Dimension d = getPreferredSize();  
+            d.height = HEIGHT;  
+            setPreferredSize(d);  
+            addMouseListener(ml);  
+            addMouseListener(listener);  
+        }  
+       
+        protected void paintComponent(Graphics g) {  
+            int w = getWidth();  
+            Graphics2D g2 = (Graphics2D)g;  
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,  
+                                RenderingHints.VALUE_ANTIALIAS_ON);  
+            g2.setPaint(new GradientPaint(w/2, 0, c1, w/2, HEIGHT/2, c2));  
+            g2.fillRect(0,0,w,HEIGHT);  
+        }  
+       
+        private MouseListener listener = new MouseAdapter() {  
+            public void mouseEntered(MouseEvent e) {  
+                titleLabel.setForeground(rolloverFg);  
+            }  
+       
+            public void mouseExited(MouseEvent e) {  
+                titleLabel.setForeground(fontFg);  
+            }  
+        };  
+    }   

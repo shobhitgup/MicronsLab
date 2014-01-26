76a8a5f3b7f1d3505287336f00a067622392d4ab
shobhitgup <shobhitgup@gmail.com>
2014-01-26 21:27:26 +0100

    b

--- JTable/src/CMenu.java
+++ JTable/src/CMenu.java
@@ -15,9 +15,11 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 import javax.swing.JTable;
 import java.awt.event.ActionListener;
@@ -72,6 +74,7 @@ public class CMenu implements SwingConstants {
 
 		JPanel leftComponent = new JPanel(new MigLayout("", "[grow][][][]", "[][][grow]"));
 		final JPanel rightComponent = new JPanel(new MigLayout("", "[grow]", "[grow]"));
+		//final JPanel rightComponent = new JPanel(new CardLayout());
 
 		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Object Repository");
 		final DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
@@ -80,91 +83,78 @@ public class CMenu implements SwingConstants {
 		tree.getSelectionModel().setSelectionMode
 		        (TreeSelectionModel.SINGLE_TREE_SELECTION);
 		tree.setShowsRootHandles(true);
+		tree.setEditable(true);
 		leftComponent.add(new JScrollPane(tree), "cell 0 2 2 1,grow");
+		
+		  
 		
 		
 		
 		tree.addMouseListener(new MouseAdapter() {
+			
+			public void valueChanged(TreeSelectionEvent e)  
+		    {  
+		        TreePath oldPath = e.getOldLeadSelectionPath();  
+		        TreePath newPath = e.getNewLeadSelectionPath();  
+		        System.out.println(oldPath);
+		        System.out.println(newPath);
+		        
+		        
+		    }  
+			
             @Override
             public void mouseReleased(MouseEvent e) {
-
-                if (e.isPopupTrigger() && e.getComponent() instanceof JTree ) {
                 	final DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-                	System.out.println(selectedNode);
-                	
                 	try {
          			   if(!selectedNode.isRoot() && selectedNode.isLeaf()){
-         				  
-         				  final JPopupMenu popup = new JPopupMenu();
-                          final JMenuItem addprop = new JMenuItem("Add Property");
-                          popup.add(addprop);
-                          popup.show(e.getComponent(), e.getX(), e.getY());
-                          addprop.addActionListener(  
-                          		  new ActionListener() {  
-                          		     
-                          		   //Handle JMenuItem event if mouse is clicked.  
-                          		   public void actionPerformed(ActionEvent event) {  
-                          		    //Display this message using JLabel if selected JMenuItem is clicked 
-                          			 DefaultTableModel model ;
-                          			String[] columnNames = {"ObjectName","Identifier1","Identifier2","Identifier3", "Identifier4", "Identifier5","Identifier6","Identifier7"};
-                          			Object[][] data = {{"ObjectName","1"}, {"ObjectName","2"}, {"ObjectName","3"}};
-                          	        model = new DefaultTableModel(data, columnNames);
-                          	        table = new JTable(model);
-                          	        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
-                          	        table.setFillsViewportHeight(true);
-                          	        scrollPane = new JScrollPane(table);
-                          	        scrollPane.setVisible(true);
-                          	        rightComponent.add(scrollPane ,"cell 0 0,grow");
-                          	        rightComponent.revalidate();
-                          	      
-                          	        
-                          	        
-                          	        table.addMouseListener(new MouseAdapter() {
-                                      @Override
-                                      public void mouseReleased(MouseEvent e) {
-                                          int r = table.rowAtPoint(e.getPoint());
-                                          if (r >= 0 && r < table.getRowCount()) {
-                                              table.setRowSelectionInterval(r, r);
-                                          } else {
-                                              table.clearSelection();
-                                          }
+         				  if (e.getClickCount() == 2){
+         					 
+         					 DefaultTableModel model ;
+                   			String[] columnNames = {"ObjectName","Identifier1","Identifier2","Identifier3", "Identifier4", "Identifier5","Identifier6","Identifier7"};
+                   			Object[][] data = {{"ObjectName","1"}, {"ObjectName","2"}, {"ObjectName","3"}};
+                   			
+                   	        model = new DefaultTableModel(data, columnNames);
+                   	        table = new JTable(model);
+                   	        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+                   	        table.setFillsViewportHeight(true);
+                   	        scrollPane = new JScrollPane(table);
+                   	        rightComponent.add(scrollPane ,"cell 0 0,grow");
+                   	        rightComponent.revalidate();
+                   	        
+                   	        table.addMouseListener(new MouseAdapter() {
+                               @Override
+                               public void mouseReleased(MouseEvent e) {
+                                   int r = table.rowAtPoint(e.getPoint());
+                                   if (r >= 0 && r < table.getRowCount()) {
+                                       table.setRowSelectionInterval(r, r);
+                                   } else {
+                                       table.clearSelection();
+                                   }
 
-                                          int rowindex = table.getSelectedRow();
-                                          if (rowindex < 0)
-                                              return;
-                                          if (e.isPopupTrigger() && e.getComponent() instanceof JTable ) {
-                                              JPopupMenu popup = new JPopupMenu();
-                                              JMenuItem mntmCopyRow = new JMenuItem("Copy Row");
-                                              JMenuItem mntmCopyRow1 = new JMenuItem("Delete Row");
-                                              popup.add(mntmCopyRow);
-                                              popup.add(mntmCopyRow1);
-                                              popup.show(e.getComponent(), e.getX(), e.getY());
-                                          }
-                                      }
-                                  });
-                          	        //scrollPane.setVisible(false);
-                          		   }  
-                          		  }  
-                          		  );}
-         			   else{
-         			
-         			   }} catch (NullPointerException f)
-         			   {
-         				   System.out.println("S");
-                       	//selectedNode.isRoot();
-                            
-         			   }
-                	
-                	
- 
-                    
-                    
-                }
+                                   int rowindex = table.getSelectedRow();
+                                   if (rowindex < 0)
+                                       return;
+                                   if (e.isPopupTrigger() && e.getComponent() instanceof JTable ) {
+                                       JPopupMenu popup = new JPopupMenu();
+                                       JMenuItem mntmCopyRow = new JMenuItem("Copy Row");
+                                       JMenuItem mntmCopyRow1 = new JMenuItem("Delete Row");
+                                       popup.add(mntmCopyRow);
+                                       popup.add(mntmCopyRow1);
+                                       popup.show(e.getComponent(), e.getX(), e.getY());
+                                   }
+                               }});
+         				  } 
+         				  }else
+         				  {
+         	        			scrollPane.setVisible(false);
+         	        			rightComponent.remove(scrollPane);
+         	        			rightComponent.revalidate();
+         				  }
+         			   } catch (NullPointerException f)
+         			   {}
+                
             }
         });
-		
-		
-		
 		
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
 				leftComponent, rightComponent);
@@ -173,16 +163,12 @@ public class CMenu implements SwingConstants {
 	    addButton.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
 				DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-				System.out.println(node);
-			    
 			    if (node == null) {
 			      JOptionPane.showMessageDialog(tree, "Select a parent.", "Error",
 			          JOptionPane.ERROR_MESSAGE);
 			      return;
 			    }
-			    
-			    String name = JOptionPane.showInputDialog(tree, "Enter Name:");
-			    treeModel.insertNodeInto(new DefaultMutableTreeNode(name), node, node.getChildCount());
+			    treeModel.insertNodeInto(new DefaultMutableTreeNode("New"), node, node.getChildCount());
 	    	}});
 	    		
 		leftComponent.add(addButton, "flowx,cell 0 0, width 60!");
@@ -203,14 +189,8 @@ public class CMenu implements SwingConstants {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setEditable(true);
 		leftComponent.add(comboBox, "cell 0 1,growx");
-		
-		
-		
-        
-        /* http://stackoverflow.com/questions/3558293/java-swing-jtable-right-click-menu-how-do-i-get-it-to-select-aka-highlight-t */
-        
-        
-        
+
+		/* http://stackoverflow.com/questions/3558293/java-swing-jtable-right-click-menu-how-do-i-get-it-to-select-aka-highlight-t */
         
 		tp.addTab(null, icon, splitPane);
 		VTextIcon textIcon2 = new VTextIcon(panel, "Requirements",  VTextIcon.ROTATE_LEFT);
@@ -221,11 +201,4 @@ public class CMenu implements SwingConstants {
 		panel.add(tp);
 		return panel;
 	}
-	
-	
-	
-	
-	
-	
 }
-
